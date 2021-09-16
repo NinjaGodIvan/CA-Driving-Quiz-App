7e30b1e41475474e5c8340a464aaac625579d7fd
Ivan Baradi <ivan4ubc@yahoo.com>
2021-09-16 13:56:40 -0700

    edited files

--- quizzes/quiz3.js
+++ quizzes/quiz3.js
@@ -3,40 +3,40 @@ module.exports = {
     type: 'true or false',
     problems: [
         {
-            question: '',
-            answer: ''
+            question: 'At a four-way all-stop intersection, the first car to arrive is the first to cross the intersection.',
+            answer: 'True'
         },
         {
-            question: '',
-            answer: ''
+            question: 'A flash yellow left-arrow light means you must yield cars at the opposite side of the road before turning left.',
+            answer: 'True'
         },
         {
-            question: '',
-            answer: ''
+            question: 'The blue line at the curb designates the driver with a disability permit can park at that curb.',
+            answer: 'True'
         },
         {
-            question: '',
-            answer: ''
+            question: 'Drivers can immediately go once the solid green light illuminates.',
+            answer: 'False'
         },
         {
-            question: '',
-            answer: ''
+            question: 'Drivers must use low-beam headlights to drive at night.',
+            answer: 'True'
         },
         {
-            question: '',
-            answer: ''
+            question: 'Drivers are not allowed to turn right against a solid red light.',
+            answer: 'False'
         },
         {
-            question: '',
-            answer: ''
+            question: 'To turn left at a T-intersection, the driver only has to look left and right.',
+            answer: 'False'
         },
         {
-            question: '',
-            answer: ''
+            question: 'To change lanes to the right, the driver must turn on the right signal, look over the right shoulder, and change lanes safely.',
+            answer: 'True'
         },
         {
-            question: '',
-            answer: ''
+            question: 'It is illegal to answer the call while driving.',
+            answer: 'True'
         },
         {
             question: '',
